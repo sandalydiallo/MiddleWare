8c8fdbb974ea0a01032b3178d3d5f47727676fac
sandalydiallo <odiallo58@gmail.com>
2018-05-01 10:44:32 +0200

    Speech to ok

--- Mp3PlayerMiddleWare/app/src/main/java/com/mp3playermiddleware/MainActivity.java
+++ Mp3PlayerMiddleWare/app/src/main/java/com/mp3playermiddleware/MainActivity.java
@@ -1,23 +1,15 @@
 package com.mp3playermiddleware;
 
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
+
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-    private static final int REQ_CODE_SPEECH_INPUT= 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,13 +34,13 @@ public class MainActivity extends AppCompatActivity {
                     switch (item.getItemId()) {
                         case R.id.navigation_home:
                             selectedFragment = new HomeFragment();
+
                             break;
                         case R.id.navigation_favorities:
                             selectedFragment = new FavoritesFragment();
                             break;
                         case R.id.navigation_speechtotext:
-                            selectedFragment = new SpeachToTextFragment();
-                            //startVoiceInput();
+                            selectedFragment = new SpeechToTextFragment();
                             break;
                     }
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
@@ -56,34 +48,4 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 }
      };
-
-    private void startVoiceInput() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Bonjour, Parler pour lancer la musique");
-        try {
-            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
-        } catch (ActivityNotFoundException a) {
-
-        }
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case REQ_CODE_SPEECH_INPUT: {
-                if (resultCode == RESULT_OK && null != data) {
-                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    mTextMessage.setText(result.get(0));
-                }
-                break;
-            }
-
-        }
-    }
-
-
-
 }
